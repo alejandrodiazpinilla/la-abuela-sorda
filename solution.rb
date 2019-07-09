9da7c3e6883062e11554a59b90f5efcc46196d97
alejandrodiazpinilla <diazjohn83@gmail.com>
2019-07-09 10:57:43 -0500

    solucion make

--- solution.rb
+++ solution.rb
@@ -8,3 +8,14 @@ post '/sorda' do
     @sorda = params[:sorda]    
     erb :sorda    
 end
+
+# Solucion maket 
+
+# post '/grandma' do
+#     if params[:user_input] == params[:user_input].upcase
+#       @grandma = "Ahhh si, manzanas!"
+#     else
+#       @grandma = "Habla más duro mijito"
+#     end
+#     redirect "/?grandma=#{@grandma}"
+#   end
